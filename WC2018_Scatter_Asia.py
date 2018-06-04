65af14589cc9ff7340a31b81820bba7dbb4a4b6a
Kashnichanoise212 <npslk.suser@gmail.com>
2018-06-04 15:28:45 +0900

    Update WC2018_Scatter_Asia.py
    added some comments etc

--- WC2018_Scatter_Asia.py
+++ WC2018_Scatter_Asia.py
@@ -8,7 +8,7 @@ import plotly
 import plotly.graph_objs as graph
 plotly.offline.init_notebook_mode(connected=False)
 
-
+#disc size: Average = 25, others = 15
 def sizes(a):
     size = df["association"]==a
     lengs=size.sum()
@@ -18,13 +18,14 @@ def sizes(a):
     sizes.append(25)
     return(sizes)
 
-
-
+#reading the file
 df = pd.read_csv("afc_Wc2018.csv")
 df.drop(columns=["age", "birth date"],  inplace=True)
 df.rename(columns={'age2': 'age'},inplace=True)
 
+#opacity
 Opacity = 0.7
+#disc sizes
 len_aus=sizes("AUS")
 len_irn=sizes("IRN")
 len_jpn=sizes("JPN")
@@ -39,8 +40,8 @@ trace4 = graph.Scatter(x = df["age"][df['association'] == 'KSA'], y = df["height
 
 
 layout = graph.Layout(
-    width=800*1.2,
-    height=450*1.2,
+    width=960,
+    height=540,
     xaxis = dict(title="age", range = [15,40], dtick=10),
     yaxis = dict(title="height", range = [160,200], dtick=10))
 
@@ -50,3 +51,6 @@ fig = dict(data = [trace0, trace1, trace2, trace3, trace4], layout = layout)
 
 
 plotly.offline.plot(fig)
+
+#for Jupyter notebook
+#plotly.offline.iplot(fig)
