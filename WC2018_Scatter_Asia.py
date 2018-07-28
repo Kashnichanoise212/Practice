e4119b372ed7e81195097804e217d9ce2f71e4ea
Kashnichanoise212 <npslk.suser@gmail.com>
2018-07-29 04:33:18 +0900

    Updated the program & the file
    1. Changed file name
    2. Updated some players' data(based on FIFA)

--- WC2018_Scatter_Asia.py
+++ WC2018_Scatter_Asia.py
@@ -1,14 +1,12 @@
 
 # coding: utf-8
 
-
-
 import pandas as pd
 import plotly
 import plotly.graph_objs as graph
 plotly.offline.init_notebook_mode(connected=False)
 
-#disc size: Average = 25, others = 15
+#Function: marker size
 def sizes(a):
     size = df["association"]==a
     lengs=size.sum()
@@ -18,20 +16,19 @@ def sizes(a):
     sizes.append(25)
     return(sizes)
 
-#reading the file
-df = pd.read_csv("afc_Wc2018.csv")
+#Reading CSV
+df = pd.read_csv("AFC_WC2018_Players_data.csv")
 df.drop(columns=["age", "birth date"],  inplace=True)
 df.rename(columns={'age2': 'age'},inplace=True)
 
-#opacity
 Opacity = 0.7
-#disc sizes
 len_aus=sizes("AUS")
 len_irn=sizes("IRN")
 len_jpn=sizes("JPN")
 len_kor=sizes("KOR")
 len_ksa=sizes("KSA")
 
+#Graph
 trace0 = graph.Scatter(x = df["age"][df['association'] == 'AUS'], y = df["height"][df['association'] == 'AUS'], mode = 'markers', text= df["name"][df['association'] == 'AUS'], name="AUS", marker=dict(size =len_aus, color="#FEC34F"), opacity=Opacity)
 trace1 = graph.Scatter(x = df["age"][df['association'] == 'IRN'], y = df["height"][df['association'] == 'IRN'], mode = 'markers', text= df["name"][df['association'] == 'IRN'],name="IRN",  marker=dict(size =len_irn, color="#BE81F7"), opacity=Opacity)
 trace2 = graph.Scatter(x = df["age"][df['association'] == 'JPN'], y = df["height"][df['association'] == 'JPN'], mode = 'markers', text= df["name"][df['association'] == 'JPN'], name="JPN", marker=dict(size =len_jpn, color="#295DF8"), opacity=Opacity)
@@ -39,16 +36,9 @@ trace3 = graph.Scatter(x = df["age"][df['association'] == 'KOR'], y = df["height
 trace4 = graph.Scatter(x = df["age"][df['association'] == 'KSA'], y = df["height"][df['association'] == 'KSA'], mode = 'markers', text= df["name"][df['association'] == 'KSA'], name="KSA", marker=dict(size =len_ksa, color="#027D46"), opacity=Opacity)
 
 
-layout = graph.Layout(
-    width=960,
-    height=540,
-    xaxis = dict(title="age", range = [15,40], dtick=10),
-    yaxis = dict(title="height", range = [160,200], dtick=10))
-
+layout = graph.Layout(width=800*1.2, height=450*1.2, xaxis = dict(title="age", range = [15,40], dtick=10), yaxis = dict(title="height", range = [160,200], dtick=10))
 
 fig = dict(data = [trace0, trace1, trace2, trace3, trace4], layout = layout)
-
-
 
 plotly.offline.plot(fig)
 
